a5f1b318bb084c4fe3b41a1d3d660d7434751140
noe <noe.reyes92@yahoo.com>
2021-08-05 11:28:16 -0500

    commit tree 4c355b33fb93a991d8c935d75508a0ee679d1c01

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Add your functions here
+function map(source, callback) {
+    const ar = [];
+
+    for (let i = 0; i < source.length; i++) {
+        const theElement = source[i];
+        ar.push(callback(theElement));
+    }
+    return ar;
+}
+
+function reduce(source, callback, startingPoint) {
+    let total;
+    if (startingPoint) {
+        total = startingPoint;
+        for (let i = 0; i < source.length; i++) {
+            total = callback(source[i], total);
+        }
+        return total;
+    } else {
+        total = source[0];
+        for (let i = 1; i < source.length; i++) {
+            total = callback(source[i], total);
+        }
+        return total;
+    }
+}
